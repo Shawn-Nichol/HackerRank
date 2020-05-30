6dd9fe7f477b918f7bb173dea20a63f41d3093a3
Shawn <7067shawnnichol@gmail.com>
2020-05-30 11:59:31 -0600

    DiagonalDifference matrix examples

---
+++ src/DiagonalDifference.kt
@@ -0,0 +1,80 @@
+fun main() {
+
+    matrix1()
+    matrix2()
+    nestedMatrix()
+    nestedMatrix2()
+    contentDeep()
+
+}
+
+fun matrix1() {
+
+    val arr = arrayOf(
+        arrayOf(1, 2, 3),
+        arrayOf(4, 5, 6, 7),
+        arrayOf(8, 9)
+    )
+
+    for (i in arr.indices) {
+        println(arr[i].contentToString())
+    }
+}
+
+fun matrix2() {
+    println("Matrix 2")
+    val arr = arrayOf(
+        arrayOf(1, 2, 3),
+        arrayOf(4, 5, 6),
+        arrayOf(7, 8, 9)
+    )
+
+    for (row in arr) {
+        println(row?.contentToString())
+    }
+}
+
+fun nestedMatrix() {
+    println("Nested Matrix")
+    val arr = arrayOf(
+        arrayOf(1, 2, 3),
+        arrayOf(4, 5, 6),
+        arrayOf(7, 8, 9)
+    )
+
+    for (row in arr) {
+        for (col in row) {
+            print("$col ")
+        }
+        println()
+    }
+}
+
+fun nestedMatrix2() {
+    println("Foreach nestMatrix")
+    val arr = arrayOf(
+        arrayOf(1, 2, 3),
+        arrayOf(4, 5, 6),
+        arrayOf(7, 8, 9)
+    )
+
+    for (row in arr) {
+        for (col in row) {
+            print("$col ")
+        }
+        println()
+    }
+}
+
+fun contentDeep() {
+    println("Content Deep to String")
+    val arr = arrayOf(
+        arrayOf(1,2,3),
+        arrayOf(4,5,6),
+        arrayOf(7,8,9)
+
+    )
+
+    println(arr.contentDeepToString())
+
+}
